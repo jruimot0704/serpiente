b2a6d65d2168d82f43f9f98362af22baf6e2ac78
jruimot0704 <122780882+jruimot0704@users.noreply.github.com>
2023-02-06 10:06:39 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,15 @@
+let a = game.createSprite(2, 2)
+basic.pause(1000)
+a.move(1)
+basic.pause(1000)
+a.set(LedSpriteProperty.X, 0)
+a.move(1)
+basic.pause(1000)
+a.turn(Direction.Right, 90)
+a.move(1)
+basic.pause(1000)
+a.turn(Direction.Right, 90)
+a.move(1)
+basic.forever(function () {
+	
+})
